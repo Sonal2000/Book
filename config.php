9fba11f228592b05ab3fa238b2f892ba6cefcc35
Sonal2000 <63189305+Sonal2000@users.noreply.github.com>
2020-07-02 23:10:37 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+//this file contains database configuration
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'login');
+
+//try connecting to database
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//check connection
+if($conn == false)
+{
+    dir('Error: Cannot connect');
+}
+?>
